cb3ccb16bdfb7ea8e5844868233da50f4b4024ef
unknown <Bruno@Bruno-PC.(none)>
2012-11-04 03:36:18 -0200

    Added DLC Packager

---
+++ RocksmithDLCPackager/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace RocksmithDLCPackager
+{
+    partial class asd2134tc3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(asd2134tc3));
+            this.asdasfsasdagq3wr = new System.Windows.Forms.Button();
+            this.asd324fdd34 = new System.Windows.Forms.Button();
+            this.ck3249wkjrhi532nk = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // asdasfsasdagq3wr
+            // 
+            this.asdasfsasdagq3wr.Location = new System.Drawing.Point(119, 56);
+            this.asdasfsasdagq3wr.Name = "asdasfsasdagq3wr";
+            this.asdasfsasdagq3wr.Size = new System.Drawing.Size(75, 23);
+            this.asdasfsasdagq3wr.TabIndex = 0;
+            this.asdasfsasdagq3wr.Text = "Pack";
+            this.asdasfsasdagq3wr.UseVisualStyleBackColor = true;
+            this.asdasfsasdagq3wr.Click += new System.EventHandler(this.asdasfsasdagq3wr_Click);
+            // 
+            // asd324fdd34
+            // 
+            this.asd324fdd34.Location = new System.Drawing.Point(119, 85);
+            this.asd324fdd34.Name = "asd324fdd34";
+            this.asd324fdd34.Size = new System.Drawing.Size(75, 23);
+            this.asd324fdd34.TabIndex = 1;
+            this.asd324fdd34.Text = "Unpack";
+            this.asd324fdd34.UseVisualStyleBackColor = true;
+            this.asd324fdd34.Click += new System.EventHandler(this.asd324fdd34_Click);
+            // 
+            // ck3249wkjrhi532nk
+            // 
+            this.ck3249wkjrhi532nk.AutoSize = true;
+            this.ck3249wkjrhi532nk.Checked = true;
+            this.ck3249wkjrhi532nk.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ck3249wkjrhi532nk.Location = new System.Drawing.Point(13, 13);
+            this.ck3249wkjrhi532nk.Name = "ck3249wkjrhi532nk";
+            this.ck3249wkjrhi532nk.Size = new System.Drawing.Size(105, 17);
+            this.ck3249wkjrhi532nk.TabIndex = 2;
+            this.ck3249wkjrhi532nk.Text = "useCryptography";
+            this.ck3249wkjrhi532nk.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // asd2134tc3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(209, 151);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ck3249wkjrhi532nk);
+            this.Controls.Add(this.asd324fdd34);
+            this.Controls.Add(this.asdasfsasdagq3wr);
+            this.Name = "asd2134tc3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button asdasfsasdagq3wr;
+        private System.Windows.Forms.Button asd324fdd34;
+        private System.Windows.Forms.CheckBox ck3249wkjrhi532nk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RocksmithDLCPackager/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ComponentAce.Compression.Libs.zlib;
+using System.Security.Cryptography;
+
+namespace RocksmithDLCPackager
+{
+    public partial class asd2134tc3 : Form
+    {
+        RijndaelManaged rij;
+        public asd2134tc3()
+        {
+            InitializeComponent();
+            rij = new RijndaelManaged();
+            rij.Padding = PaddingMode.None;
+            rij.Mode = CipherMode.ECB;
+            rij.BlockSize = 128;
+            rij.IV = new byte[16];
+            rij.Key = new byte[32] { 0xFA, 0x6F, 0x4F, 0x42, 0x3E, 0x66, 0x9F, 0x9E, 0x6A, 0xD2, 0x3A, 0x2F, 0x8F, 0xE5, 0x81, 0x88, 0x63, 0xD9, 0xB8, 0xFD, 0xED, 0xDF, 0xFE, 0xBD, 0x12, 0xB2, 0x7F, 0x76, 0x80, 0xD1, 0x51, 0x41 };
+        }
+
+        private void asdasfsasdagq3wr_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            var name = GetLastDirectory(fbd.SelectedPath);
+            var psarc = new PSARC.PSARC();
+            foreach (var x in Directory.EnumerateFiles(fbd.SelectedPath))
+            {
+                var entry = new PSARC.Entry() { Name = Path.GetFileName(x), Data = File.OpenRead(x) };
+                entry.Length = (ulong)entry.Data.Length;
+                psarc.AddEntry(entry);
+            }
+            foreach (var x in Directory.EnumerateDirectories(fbd.SelectedPath))
+            {
+                var dName = GetLastDirectory(x);
+                var psarc2 = new PSARC.PSARC();
+                WalkTroughDirectory("", x, (a, b) => psarc2.AddEntry(a, File.OpenRead(b)));
+                var dt = new MemoryStream();
+                psarc2.Write(dt);
+                foreach (var y in psarc2.Entries)
+                    y.Data.Close();
+                psarc.AddEntry(dName + ".psarc", dt);
+            }
+            if (ck3249wkjrhi532nk.Checked)
+            {
+                var data = new MemoryStream();
+                psarc.Write(data);
+                data.Seek(0, SeekOrigin.Begin);
+                using (var str = File.Create(sfd.FileName))
+                    Crypto(data, str, rij.CreateEncryptor());
+            }
+            else
+                using (var str = File.Create(sfd.FileName))
+                    psarc.Write(str);
+        }
+
+        void WalkTroughDirectory(string baseDir, string directory, Action<string, string> action)
+        {
+            foreach (var fl in Directory.GetFiles(directory))
+                action(String.Format("{0}/{1}", baseDir, Path.GetFileName(fl)).TrimStart('/'), fl);
+            foreach (var dr in Directory.GetDirectories(directory))
+                WalkTroughDirectory(String.Format("{0}/{1}", baseDir, GetLastDirectory(dr)), dr, action);
+        }
+
+        string GetLastDirectory(string dir)
+        {
+            return dir.Split(new string[4] { "/", "//", @"\", @"\\" }, StringSplitOptions.RemoveEmptyEntries).Last();
+        }
+
+        private void extractPSARC(string filename, string path, Stream dt)
+        {
+            var name = Path.GetFileNameWithoutExtension(filename);
+            var psarc = new PSARC.PSARC();
+            psarc.Read(dt);
+            for (int i = 1; i < psarc.Entries.Count; i++)
+            {
+                var x = psarc.Entries[i];
+                var fullfilename = path + "\\" + name + "\\" + x.Name;
+                x.Data.Seek(0, SeekOrigin.Begin);
+                if (Path.GetExtension(x.Name).ToLower() == ".psarc")
+                {
+                    extractPSARC(fullfilename, path + "\\" + name, x.Data);
+                }
+                else
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullfilename));
+                    using (var str = File.Create(fullfilename))
+                    {
+                        var buff = new byte[x.Data.Length];
+                        x.Data.Read(buff, 0, buff.Length);
+                        str.Write(buff, 0, buff.Length);
+                        x.Data.Seek(0, SeekOrigin.Begin);
+                    }
+                }
+            }
+        }
+
+        private void Crypto(Stream input, Stream output, ICryptoTransform transform)
+        {
+            var buffer = new byte[1024];
+            var cs = new CryptoStream(output, transform, CryptoStreamMode.Write);
+            for (long i = 0; i < input.Length; i += buffer.Length)
+            {
+                int sz = (int)Math.Min(input.Length - input.Position, buffer.Length);
+                input.Read(buffer, 0, sz);
+                cs.Write(buffer, 0, sz);
+            }
+            int pad = buffer.Length - (int)(input.Length % buffer.Length);
+            if (pad > 0)
+                cs.Write(new byte[pad], 0, pad);
+            cs.Flush();
+            output.Flush();
+            output.Seek(0, SeekOrigin.Begin);
+        }
+
+        private void asd324fdd34_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            using (var str = File.OpenRead(ofd.FileName))
+            {
+                if (ck3249wkjrhi532nk.Checked)
+                {
+                    var ms = new MemoryStream((int)str.Length);
+                    Crypto(str, ms, rij.CreateDecryptor());
+                    extractPSARC(ofd.FileName, fbd.SelectedPath, ms);
+                }
+                else
+                    extractPSARC(ofd.FileName, fbd.SelectedPath, str);
+            }
+
+        }
+
+    }
+}
