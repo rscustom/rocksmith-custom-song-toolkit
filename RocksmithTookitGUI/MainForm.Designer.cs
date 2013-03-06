919fc0715106d2b7b6554677875fe48d0cf308c7
Fabiano Santiago <fabianosan2006@gmail.com>
2013-03-06 19:41:56 -0300

    Changed tab order.

--- RocksmithTookitGUI/MainForm.Designer.cs
+++ RocksmithTookitGUI/MainForm.Designer.cs
@@ -117,10 +117,10 @@
             this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.sngFileCreatorTab);
-            this.tabControl1.Controls.Add(this.oggConverterTab);
             this.tabControl1.Controls.Add(this.dlcPackageCreatorTab);
             this.tabControl1.Controls.Add(this.dlcPackerUnpackerTab);
+            this.tabControl1.Controls.Add(this.sngFileCreatorTab);
+            this.tabControl1.Controls.Add(this.oggConverterTab);
             this.tabControl1.Controls.Add(this.zigProConverterTab);
             this.tabControl1.Location = new System.Drawing.Point(17, 125);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
