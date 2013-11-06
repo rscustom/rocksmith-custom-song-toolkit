532396916dff1a146bcd87c1b330ee89f44554bb
Fabiano Santiago <fabianosan2006@gmail.com>
2013-11-06 21:37:48 -0200

    Added Showlight object to load showlights.xml

---
+++ RocksmithToolkitLib/DLCPackage/ShowlightRS2014/ShowlightRS2014.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace RocksmithToolkitLib.DLCPackage.ShowlightRS2014
+{
+    [XmlType("showlight")]
+    public class ShowlightRS2014
+    {
+        [XmlAttribute("Time")]
+        public float Time { get; set; }
+        [XmlAttribute("note")]
+        public int Note { get; set; }
+    }
+}
