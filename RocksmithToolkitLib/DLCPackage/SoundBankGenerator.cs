fc0fa71e3e984856948da684ca7c87ab7d7430b1
snakez <snakez@gmail.com>
2013-11-12 00:45:58 -0500

    Fixed bnk offset

--- RocksmithToolkitLib/DLCPackage/SoundBankGenerator.cs
+++ RocksmithToolkitLib/DLCPackage/SoundBankGenerator.cs
@@ -17,8 +17,8 @@ namespace RocksmithToolkitLib.DLCPackage
         private const string SONG = "Song_";
         private static readonly int[] bnkPCOffsets = { 0x2c, 0x1d, 0x17, 0xfa, 0xc8, 0x14, 0xc };
         private static readonly int[] bnkConsoleOffsets = { 0x7ec, 0x1d, 0x17, 0xfa, 0xc8, 0x14, 0xc };
-        private static readonly int[] bnkPC2014Offsets = { 0x2c, 0x1d, 0x1a, 0x78, 0x00, 0x12, 0xc };
-        private static readonly int[] bnkConsole2014Offsets = { 0x7ec, 0x1d, 0x1a, 0x78, 0x00, 0x12, 0xc };
+        private static readonly int[] bnkPC2014Offsets = { 0x2c, 0x1d, 0x1a, 0x78, 0x00, 0xc, 0xc };
+        private static readonly int[] bnkConsole2014Offsets = { 0x7ec, 0x1d, 0x1a, 0x78, 0x00, 0xc, 0xc };
         
         public static IList<int> GetOffsets(this Platform platform) {
             switch (platform.version)
