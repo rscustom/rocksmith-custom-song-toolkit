ccb433680aad2e43dee6a077d4f2f82ebba85697
L0FKA <ul0fka@gmail.com>
2014-01-24 03:05:25 +0200

    Temp fix for tuning import feature, note display
    Changed from # to b representation, could be forced back.

--- RocksmithToolkitLib/XmlRepository/TuningDefinition.cs
+++ RocksmithToolkitLib/XmlRepository/TuningDefinition.cs
@@ -20,22 +20,26 @@ namespace RocksmithToolkitLib.DLCPackage {
         [XmlElement]
         public TuningStrings Tuning { get; set; }
 
-        public string NameFromStrings(TuningStrings tuning, bool isBass)
+        public string NameFromStrings(TuningStrings tuning, bool isBass, bool inBem = true)
         {
             List<Int32> Notes = new List<Int32>();
             List<String> NoteNames = new List<String>();
             String[] notesNames = new String[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+            String[] notesNamesHi = new String[] { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
             for (Byte s = 0; s < 6; s++)
                 Notes.Add(Sng2014FileWriter.getMidiNote(tuning.ToShortArray(), s, 0, isBass));
             foreach (var mNote in Notes)
-                NoteNames.Add(notesNames[mNote % 12]); //oct = mNote / 12 - 1
+                if(inBem) NoteNames.Add(notesNamesHi[mNote % 12]); //oct = mNote / 12 - 1
+                else NoteNames.Add(notesNames[mNote % 12]); //oct = mNote / 12 - 1
+
 
             return String.Format("{0}{1}{2}{3}{4}{5}", NoteNames[0], NoteNames[1], NoteNames[2], 
                                                        NoteNames[3], NoteNames[4], NoteNames[5]);
         }
 
         public override string ToString() {
-            return (Custom) ? String.Format("{0} (custom)", UIName) : UIName;
+            //return (Custom) ? String.Format("{0} (custom)", UIName) : UIName;
+            return UIName;
         }
     }
 }
