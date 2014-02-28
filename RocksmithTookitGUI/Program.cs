7de3098dd750eb5b9deef601ca3c1ee5815c06b2
L0FKA <ul0fka@gmail.com>
2014-02-28 19:23:45 +0200

    Toolkit hang fixed
    Was error in String.Format() command, sorry

--- RocksmithTookitGUI/Program.cs
+++ RocksmithTookitGUI/Program.cs
@@ -30,7 +30,7 @@ namespace RocksmithToolkitGUI
             {
                 var exception = (Exception)e.ExceptionObject;
                 MessageBox.Show(String.Format("Application.ThreadException\n{0}\n{1}\nPlease send us \"_RSToolkit_{2}.log\", you can find it in Toolkit folder.",
-                    exception.ToString(), DateTime.Now.ToString("yyyy-MM-dd")), exception.Message.ToString());
+                    exception.ToString(), exception.Message.ToString(), DateTime.Now.ToString("yyyy-MM-dd")), "Unhandled Exception catched!");
                 log.ErrorException(String.Format("\n{0}\n{1}\nException catched:\n{2}\n", exception.Source, exception.TargetSite, exception.InnerException), exception);
             };
 
@@ -41,7 +41,7 @@ namespace RocksmithToolkitGUI
             {
                 var exception = (Exception)e.Exception;
                 MessageBox.Show(String.Format("Application.ThreadException\n{0}\n{1}\nPlease send us \"_RSToolkit_{2}.log\", you can find it in Toolkit folder.",
-                    exception.ToString(), DateTime.Now.ToString("yyyy-MM-dd")), exception.Message.ToString());
+                    exception.ToString(),  exception.Message.ToString(), DateTime.Now.ToString("yyyy-MM-dd")), "Thread Exception catched!");
                 log.ErrorException(String.Format("\n{0}\n{1}\nException catched:\n{2}\n", exception.Source, exception.TargetSite, exception.InnerException), exception);
             };
 
