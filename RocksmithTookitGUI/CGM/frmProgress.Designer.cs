1486ba4a6187a32dd1bfce69c3afd4f52b78b21d
Cozy1 <cozynorte@yahoo.com>
2014-03-27 07:54:31 -0600

    Added new tab for Custom Guitar Maker (CGM).  This feature allows users to modify the guitar neck inlay appearance in the game.  Custom guitar inlays are treated and used much the same way as CDLC music is.  User creates graphics for inlay and a button icon, then creates dlc/Inlay/*.psarc file.  The dlc/Inlay folder is stored in the RS2014 root folder similar to how CDLC may be stored in dlc/Customs folder.
    Users can save their artwork to a *.cgm file than then can be shared with other RS2014 users similar to CDLC sharing.  Need to establish a *.cgm file sharing method/service for users similar to CDLC.

---
+++ RocksmithTookitGUI/CGM/frmProgress.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RocksmithToolkitGUI.CGM
+{
+    partial class frmProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProgress));
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblFooter = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progBar
+            // 
+            this.progBar.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.progBar.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.progBar.Location = new System.Drawing.Point(16, 35);
+            this.progBar.MarqueeAnimationSpeed = 50;
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(197, 17);
+            this.progBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progBar.TabIndex = 0;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(14, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(202, 19);
+            this.lblHeader.TabIndex = 2;
+            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(-6, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 49);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblFooter.Location = new System.Drawing.Point(14, 71);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(202, 18);
+            this.lblFooter.TabIndex = 4;
+            this.lblFooter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(226, 99);
+            this.Controls.Add(this.lblFooter);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.progBar);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmProgress";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmProgress";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label lblHeader;
+        public System.Windows.Forms.Label lblFooter;
+ 
+    }
+}
