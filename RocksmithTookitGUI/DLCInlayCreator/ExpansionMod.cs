0c2234826d33a8b83e924b5a6b181d589365e044
cozy1 <cozynorte@yahoo.com>
2014-04-29 13:23:05 -0500

    New control for expansion

---
+++ RocksmithTookitGUI/DLCInlayCreator/ExpansionMod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RocksmithToolkitGUI.DLCInlayCreator
+{
+    public partial class ExpansionMod : UserControl
+    {
+        public ExpansionMod()
+        {
+            InitializeComponent();
+        }
+    }
+}
