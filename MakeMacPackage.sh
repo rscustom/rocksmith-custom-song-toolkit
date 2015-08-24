31eb5210c8be5c7a7ee84d3751d84949806b3e98
L0FKA <ul0fka@gmail.com>
2015-08-24 16:31:01 +0300

    Cleanup for platforms and events.

---
+++ MakeMacPackage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Mac package
+
+rm -rf "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/gitkeep.txt"
+cp -f "./RocksmithTookitGUI/bin/Release/*" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/"
+cp -f "./RocksmithTookitGUI/songcreator.icns" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/"
+cp -f "./RocksmithTookitGUI/bin/Release/RocksmithToolkitGUI.exe" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/RocksmithCustomSongToolkit.exe"
+tar -cvzf Mac.tar "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/"
+
+echo Done
+#exit /b 0
