032289bddeff5979bb22ffcdc4fc0eb4f77ef1c3
L0FKA <ul0fka@gmail.com>
2015-08-24 17:40:20 +0300

    More cleanup for the Mac package.

--- MakeMacPackage.sh
+++ MakeMacPackage.sh
@@ -6,6 +6,7 @@ rm -rf "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resourc
 cp -f "./RocksmithTookitGUI/bin/Release/*" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/"
 cp -f "./RocksmithTookitGUI/songcreator.icns" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/"
 cp -f "./RocksmithTookitGUI/bin/Release/RocksmithToolkitGUI.exe" "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/RocksmithCustomSongToolkit.exe"
+rm -rf "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/Contents/Resources/RocksmithToolkitGUI.exe"
 tar -cvzf Mac.tar "./RocksmithTookitGUI/bin/RocksmithCustomSongToolkit.app/"
 
 echo Done
