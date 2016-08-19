cbba75cbea21b542d9b20d5f1459b4c299195823
cozyuno <cozyuno@gmail.com>
2016-08-19 11:27:11 -0500

    appveyor

--- PatchAssemblyVersion.ps1
+++ PatchAssemblyVersion.ps1
@@ -1,6 +1,6 @@
-param([string]$AssemblyFile) 
+param([string]$Assembly_File) 
 
-$content = [IO.File]::ReadAllText($assemblyFile)
+$content = [IO.File]::ReadAllText($Assembly_File)
 
 $regexAIV = new-object System.Text.RegularExpressions.Regex ('(AssemblyInformationalVersion(Attribute)?\s*\(\s*\")(.*)(\"\s*\))', 
          [System.Text.RegularExpressions.RegexOptions]::MultiLine)
@@ -16,15 +16,12 @@ $Assembly_Informational_Version = "$env:GIT_HASH"
 # will be applied to all AssemblyInfo.cs files ...
 $Assembly_Version = "2.7.1.0"
 
-Write-Host "- Patching: $AssemblyFile"
+Write-Host "- Patching: $Assembly_File"
 Write-Host "- AssemblyVersion: $Assembly_Version"
-Write-Host "- AssemblyInformationVersion: $GIT_HASH"
+Write-Host "- AssemblyInformationVersion: $env:GIT_HASH"
 
 # update assembly info
 $content = $regexAIV.Replace($content, '${1}' + $Assembly_Informational_Version + '${4}')
 $content = $regexAV.Replace($content, '${1}' + $Assembly_Version + '${4}')
 
-[IO.File]::WriteAllText($assemblyFile, $content)
-
-# update AppVeyor build
-Update-AppveyorBuild -Version $version
+[IO.File]::WriteAllText($Assembly_File, $content)
