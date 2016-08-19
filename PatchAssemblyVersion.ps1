1a14c2822b4c04597b077e748fe080536dc6f93c
cozyuno <cozyuno@gmail.com>
2016-08-19 09:09:12 -0500

    test

--- PatchAssemblyVersion.ps1
+++ PatchAssemblyVersion.ps1
@@ -1,10 +1,23 @@
-Param(
-    [Parameter(Mandatory=$true)][string] $version,
-)
+$assemblyFile = "$env:APPVEYOR_BUILD_FOLDER\RocksmithToolkitLib\Properties\AssemblyInfo.cs"
 
-Write-Host "Applying version ${version} to AssemblyInfo.cs"
+$regex = new-object System.Text.RegularExpressions.Regex ('(AssemblyInformationalVersion(Attribute)?\s*\(\s*\")(.*)(\"\s*\))', 
+         [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 
-foreach ($file in "AssemblyInfo.cs" )
-{
-	$env:APPVEYOR_BUILD_FOLDER | get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceVersion $version;
+$content = [IO.File]::ReadAllText($assemblyFile)
+
+$version = $null
+$match = $regex.Match($content)
+if($match.Success) {
+    $version = $match.groups[3].value
 }
+
+# new version
+$env:GIT_HASH = $env:APPVEYOR_REPO_COMMIT.Substring(0, 8)
+$version = "$version.$env:GIT_HASH"
+
+# update assembly info
+$content = $regex.Replace($content, '${1}' + $version + '${4}')
+[IO.File]::WriteAllText($assemblyFile, $content)
+
+# update AppVeyor build
+Update-AppveyorBuild -Version $version
