2a7535993d1490d60a09243dcb43b403f3ceb624
cozyuno <cozyuno@gmail.com>
2016-10-14 17:04:13 -0500

    Refactor and fix dependencies - AppVeyour Commit #4

--- RocksmithToolkitCLI/artistfolders/Properties/Resources.Designer.cs
+++ RocksmithToolkitCLI/artistfolders/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace artistfolders.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace artistfolders.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SplitSongPack.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("artistfolders.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
