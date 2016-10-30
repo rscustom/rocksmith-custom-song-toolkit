0816151819546d417666eef902d58545beb01210
cozyuno <cozyuno@gmail.com>
2016-10-30 09:08:20 -0600

    Removed unused directive from AutoUpdater that was causing AppVeyor crash

--- RocksmithToolkitUpdater/AutoUpdater.cs
+++ RocksmithToolkitUpdater/AutoUpdater.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using RocksmithToolkitLib.XmlRepository;
 
 namespace RocksmithToolkitUpdater
 {
