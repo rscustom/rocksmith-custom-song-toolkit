73ce2bcb7e60a203de36162e064cfe4e8d96b327
cozyuno <cozyuno@gmail.com>
2016-11-14 10:45:04 -0600

    Recommit - AppVeyour did not see last commit.

--- RocksmithToolkitUpdater/Properties/AssemblyInfo.cs
+++ RocksmithToolkitUpdater/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.7.1.0")]
-[assembly: AssemblyInformationalVersion("08161518")]
+[assembly: AssemblyInformationalVersion("317e00c0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
