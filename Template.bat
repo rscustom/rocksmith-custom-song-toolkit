800d0057a798225e8a4d96447894a593e1719c20
cozyuno <cozyuno@gmail.com>
2018-09-12 14:47:02 -0500

    - Commit cleanup

--- Template.bat
+++
@@ -1,9 +0,0 @@
-RD /Q /S "Template\.cache"
-RD /Q /S "Template\GeneratedSoundBanks"
-DEL /Q ""Template\Template.Administrator.validationcache"
-
-Pause "Deleted .cache and GenerateSoundBanks directories ..."
-
-"D:\Program Files\Audiokinetic\Wwise v2010.3.3 build 3773\Authoring\Win32\Release\bin\WwiseCLI.exe" "Template\Template.wproj" -GenerateSoundBanks -Platform Windows -Language English(US) -NoWwiseDat -ClearAudioFileCache -Save -Verbose
-
-Pause "Done"
